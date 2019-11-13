9c52be25044c62b7085e2bcb8f89ce827f536105
iamasianman <kevinle1991@gmail.com>
2019-11-12 19:08:16 -0800

    implemented App with mock data

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import Jobs from './Jobs';
 
-function App() {
-  return (
-    <div className="App">
+// const mockJobs = [
+//   { title: 'SWE-1', company: 'Google' },
+//   { title: 'SWE-1', company: 'Apple' },
+//   { title: 'SWE-1', company: 'FaceBook' },
 
-    </div>
-  );
+// ]
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Jobs jobs={mockJobs} />
+//     </div>
+//   );
+// }
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.name = 'MyComponent';
+    this.handleClick1 = this.handleClick1.bind(this)
+    this.handleClick2 = this.handleClick1.bind(this);
+  }
+
+  handleClick1() {
+    alert(this.name);
+  }
+
+  handleClick3 = () => alert(this.name);
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick1()}>click 1</button>
+        <button onClick={this.handleClick1}>click 2</button>
+        <button onClick={this.handleClick2}>click 3</button>
+        <button onClick={this.handleClick3}>click 4</button>
+      </div>
+    );
+  }
 }
-
 export default App;
